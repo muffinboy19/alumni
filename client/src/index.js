8b4eed07434842b8fedee3595952e733104a67d9
muffinboy <87519281+muffinboy19@users.noreply.github.com>
2024-09-15 10:53:21 +0530

    axios

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
@@ -15,6 +16,8 @@ root.render(
   </Provider>
 );
 
+
+axios.defaults.baseURL = 'http://89.116.34.246:5000/';
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
