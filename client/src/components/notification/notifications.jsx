fc5035a9213d041ba244543ec495453009a463d5
Gamin8ing <anim8tor.ing@gmail.com>
2024-09-27 19:30:41 +0530

    test noti

--- client/src/components/notification/notifications.jsx
+++ client/src/components/notification/notifications.jsx
@@ -209,111 +209,106 @@
 
 // export default Notifications;
 
-import {
-  DropdownButton,
-  Dropdown,
-  Badge,
-} from "react-bootstrap";
+import { DropdownButton, Dropdown, Badge } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./temp.css";
 
 const Notifications = () => {
-  let userData = localStorage.getItem("_user_data");
+	let userData = localStorage.getItem("_user_data");
 
-  let userID;
-  let userName;
-  let userRole;
+	let userID;
+	let userName;
+	let userRole;
 
-  if (userData && userData !== "none") {
-    userData = JSON.parse(userData);
-    userID = userData._id;
-    userName = userData.name;
-    userRole = userData.role;
-  }
+	if (userData && userData !== "none") {
+		userData = JSON.parse(userData);
+		userID = userData._id;
+		userName = userData.name;
+		userRole = userData.role;
+	}
 
-  const [noti, setNotifications] = useState([]);
-  const [notiID, setNotiID] = useState(0);
-  const [newNotifications, setNewNotifications] = useState(0);
-  const [totalNotifications, setTotalNotifications] = useState(0); // New state for total notifications
+	const [noti, setNotifications] = useState([]);
+	const [notiID, setNotiID] = useState(0);
+	const [newNotifications, setNewNotifications] = useState(0);
+	const [totalNotifications, setTotalNotifications] = useState(0); // New state for total notifications
 
-  useEffect(() => {
-    const getData = async () => {
-      if (!userID) return; // Make sure userID is valid before making the request
+	useEffect(() => {
+		const getData = async () => {
+			if (!userID) return; // Make sure userID is valid before making the request
 
-      const tosend = {
-        userID: userID,
-      };
+			const tosend = {
+				userID: userID,
+			};
 
-      try {
-        const data = await axios.post(
-          "/api/notifications/getNotifications",
-          tosend
-        );
+			try {
+				const data = await axios.post(
+					"/api/notifications/getNotifications",
+					tosend
+				);
 
-        if (data && data.data && data.data.data) {
-          const notifications = data.data.data.reverse();
-          setNotifications(notifications);
-          setNotiID(data.data.ID.id);
-          setNewNotifications(parseInt(notifications.length - data.data.ID.id));
-          setTotalNotifications(notifications.length); // Set the total notifications length
-        }
-      } catch (error) {
-        console.error("Error fetching notifications", error);
-      }
-    };
+				if (data && data.data && data.data.data) {
+					console.log(data);
+					const notifications = data.data.data.reverse();
+					setNotifications(notifications);
+					setNotiID(data.data.ID.id);
+					setNewNotifications(parseInt(notifications.length - data.data.ID.id));
+					setTotalNotifications(notifications.length); // Set the total notifications length
+				}
+			} catch (error) {
+				console.error("Error fetching notifications", error);
+			}
+		};
 
-    getData();
-  }, [userID]);
+		getData();
+	}, [userID]);
 
-  const clicked = async () => {
-    if (!userID) return;
+	const clicked = async () => {
+		if (!userID) return;
 
-    const tosend = {
-      userID: userID,
-    };
+		const tosend = {
+			userID: userID,
+		};
 
-    try {
-      await axios.post("/api/notifications/updateId", tosend);
-       setNewNotifications(0);
-    } catch (error) {
-      console.error("Error updating notifications", error);
-    }
-  };
+		try {
+			await axios.post("/api/notifications/updateId", tosend);
+			setNewNotifications(0);
+		} catch (error) {
+			console.error("Error updating notifications", error);
+		}
+	};
 
-  return (
-    <>
-      <DropdownButton
-        size="sm"
-        title={`${totalNotifications} 🔔`} // Display the total number of notifications
-        onClick={clicked}
-        id="dropdown-size-small"
-      >
-        {noti.map((ele, i) => (
-          <Dropdown.Item
-            key={i}
-            href={ele.Notification_link}
-            style={{
-              margin: "5px",
-              backgroundColor: "#e4e0e0",
-              width: "350px",
-              whiteSpace: "normal",
-              wordBreak: "break-word",
-            }}
-          >
-            {i < newNotifications ? <Badge bg="success">New</Badge> : null}
-            <div className="texth1">
-              Hi there, a new <b>{ele.Notification_name}</b> is available
-            </div>
-            Date: {ele.Notification_post_date.slice(0, 10)}{" "}
-            {ele.Notification_post_date.slice(11, 16)}
-          </Dropdown.Item>
-        ))}
-      </DropdownButton>
-    </>
-  );
+	return (
+		<>
+			<DropdownButton
+				size="sm"
+				title={`${totalNotifications} 🔔`} // Display the total number of notifications
+				onClick={clicked}
+				id="dropdown-size-small"
+			>
+				{noti.map((ele, i) => (
+					<Dropdown.Item
+						key={i}
+						href={ele.Notification_link}
+						style={{
+							margin: "5px",
+							backgroundColor: "#e4e0e0",
+							width: "350px",
+							whiteSpace: "normal",
+							wordBreak: "break-word",
+						}}
+					>
+						{i < newNotifications ? <Badge bg="success">New</Badge> : null}
+						<div className="texth1">
+							Hi there, a new <b>{ele.Notification_name}</b> is available
+						</div>
+						Date: {ele.Notification_post_date.slice(0, 10)}{" "}
+						{ele.Notification_post_date.slice(11, 16)}
+					</Dropdown.Item>
+				))}
+			</DropdownButton>
+		</>
+	);
 };
 
 export default Notifications;
-
-
